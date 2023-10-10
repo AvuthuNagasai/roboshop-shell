1e49fdf28871d26e441b2697ef8253578322bb17
nagasai <avuthunagasaireddy@gmail.com>
2023-10-10 06:22:20 +0530

    learning front end automation

--- frontend.sh
+++ frontend.sh
@@ -4,13 +4,13 @@ echo -e "\e[31mremoving old app content\e[om"
 rm -rf /usr/share/nginx/html/* >/tmp/roboshop.log
 
 echo -e "\e[32mdownloading frontend component\e[om"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip >/tmp/roboshop.log
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip >/tmp/roboshop.log 2>/temp/error.log
 
 echo -e "\e[33mextract frontend content\e[om"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip >/tmp/roboshop.log
 ## we need to copy config file
 
-echo -e "\e[33mstarting nginx content\e[om"
+echo -e "\e[34mstarting nginx content\e[om"
 systemctl enable nginx
 systemctl start nginx
