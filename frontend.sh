ea123766f0c3d8bb3c4d391d2a6f451b3c919038
nagasai <avuthunagasaireddy@gmail.com>
2023-10-10 10:45:21 +0530

    learning front end automation

--- frontend.sh
+++ frontend.sh
@@ -1,16 +1,18 @@
-dnf install nginx -y &>>/tmp/roboshop.log
+echo -e "\e[31m Installing NGINX \e[0m"
+yum install nginx -y
 
-echo -e "\e[31m removing old app content\e[om"
-rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
+echo -e "\e[32m removing old app content\e[0m"
+rm -rf /usr/share/nginx/html/*
 
-echo -e "\e[32m downloading frontend component\e[om"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/roboshop.log
+echo -e "\e[33m removing old app content\e[0m"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
-echo -e "\e[33m extract frontend content\e[om"
+echo -e "\e[34m extracting frontend content\e[0m"
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip &>>/tmp/roboshop.log
+unzip /tmp/frontend.zip
+
 ## we need to copy config file
 
-echo -e "\e[34m starting nginx content\e[om"
-systemctl enable nginx &>>/tmp/roboshop.log
-systemctl start nginx &>>/tmp/roboshop.log
+echo -e "\e[31m starting NGINX server\e[0m"
+systemctl enable nginx
+systemctl start nginx
