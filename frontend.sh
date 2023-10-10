0a69c7e6745030f01b1f54136ce172eecbeb9fdc
nagasai <avuthunagasaireddy@gmail.com>
2023-10-10 11:00:27 +0530

    learning front end automation

--- frontend.sh
+++ frontend.sh
@@ -1,13 +1,13 @@
 echo -e "\e[31m Installing NGINX \e[0m"
 yum install nginx -y &>>/tmp/roboshop.log
 
-echo -e "\e[32m removing old app content\e[0m"
+echo -e "\e[31m removing old app content\e[0m"
 rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
 
-echo -e "\e[33m download frontend content\e[0m"
+echo -e "\e[31m download frontend content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>/tmp/roboshop.log
 
-echo -e "\e[34m extracting frontend content\e[0m"
+echo -e "\e[31m extracting frontend content\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>/tmp/roboshop.log
 
