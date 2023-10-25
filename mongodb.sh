c3aa4a8725af75f176e35c91f714cbd0b64278f1
nagasai <avuthunagasaireddy@gmail.com>
2023-10-25 16:52:25 +0530

    learning front end automation

--- mongodb.sh
+++ mongodb.sh
@@ -2,11 +2,11 @@ echo -e "\e[32mcopying mongodb repo file \e[0m"
 
 cp mongodb.repo /etc/yum.repos.d/mongo.repo
 
-echo -e "\e[33m Mongodb service \e[0m"
+echo -e "\e[33m install Mongodb server \e[0m"
 dnf install mongodb-org -y &>>/tmp/roboshop.log
 
 ## Modify the config file
 
-echo -e "\e[35m MongoDB service \e[0m"
+echo -e "\e[35m start MongoDB service \e[0m"
 systemctl enable mongod &>>/tmp/roboshop.log
 systemctl start mongod &>>/tmp/roboshop.log
