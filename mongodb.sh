a08e0292e2e4b405d17db223632dedb000a6c4a5
nagasai <avuthunagasaireddy@gmail.com>
2024-04-04 10:15:47 +0530

    learning front end automation

--- mongodb.sh
+++ mongodb.sh
@@ -6,7 +6,7 @@ echo -e "\e[33m install Mongodb server \e[0m"
 dnf install mongodb-org -y &>>/tmp/roboshop.log
 
 ## Modify the config file
-echo -e "\e[34m updating mongodb d=server listen address \e[0m"
+echo -e "\e[34m updating mongodb server listen address \e[0m"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongodb.conf
 
 echo -e "\e[35m start mongoDB service \e[0m"
