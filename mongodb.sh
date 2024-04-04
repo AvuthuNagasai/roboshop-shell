9a2a721c1378f78ab1cacbc3b415712dbddb3662
nagasai <avuthunagasaireddy@gmail.com>
2024-04-04 10:58:28 +0530

    learning front end automation

--- mongodb.sh
+++ mongodb.sh
@@ -2,10 +2,13 @@ echo -e "\e[32m copying mongodb repo file \e[0m"
 
 cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
 
-echo -e "\e[33m install Mongodb server \e[0m"
+echo -e "\e[34m install Mongodb server \e[0m"
 dnf install mongodb-org -y &>>/tmp/roboshop.log
 
 ## Modify the config file
+
+echo -e "\e[33m updating mongodb server listen address \e[0m"
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 echo -e "\e[35m start mongodb service \e[0m"
 systemctl enable mongod &>>/tmp/robooshop.log
