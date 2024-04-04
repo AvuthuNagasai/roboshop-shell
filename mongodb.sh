3a8406a82ea565438b8428fee8cb71120b3c55b8
nagasai <avuthunagasaireddy@gmail.com>
2024-04-04 10:13:49 +0530

    learning front end automation

--- mongodb.sh
+++ mongodb.sh
@@ -3,7 +3,7 @@ echo -e "\e[32m copying mongodb repo file \e[0m"
 cp mongodb.repo /etc/yum.repos.d/mongodb.repo
 
 echo -e "\e[33m install Mongodb server \e[0m"
-dnf install mongodb-org -y
+dnf install mongodb-org -y &>>/tmp/roboshop.log
 
 ## Modify the config file
 echo -e "\e[34m updating mongodb d=server listen address \e[0m"
