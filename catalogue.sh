9ee28af38b9fdd6009cbd2ec479e565f98528dd9
nagasai <avuthunagasaireddy@gmail.com>
2024-04-04 13:00:39 +0530

    learning front end automation

--- catalogue.sh
+++ catalogue.sh
@@ -1,28 +1,28 @@
 echo -e "\e[34m enabling nodejs version 18 \e[0m"
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
+dnf module disable nodejs -y &>>/tmp/roboshop.log
+dnf module enable nodejs:18 -y &>>/tmp/roboshop.log
 echo -e "\e[34m installing nodejs \e[0m"
-dnf install nodejs -y
+dnf install nodejs -y &>>/tmp/roboshop.log
 echo -e "\e[34m adding user roboshop \e[0m"
-useradd roboshop
+useradd roboshop &>>/tmp/roboshop.log
 echo -e "\e[34m create app directory \e[0m"
-mkdir /app
+mkdir /app &>>/tmp/roboshop.log
 echo -e "\e[34m Download the application code to created app directory \e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
 cd /app
-unzip /tmp/catalogue.zip
+unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
 echo -e "\e[34m download the dependencies \e[0m"
 cd /app
-npm install
+npm install &>>/tmp/roboshop.log
 echo -e "\e[34m Setting up SystemD Catalogue Service \e[0m"
-cp catalogue.service /etc/systemd/system/catalogue.service
+cp catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
 echo -e "\e[34m starting catalogue \e[0m"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
 echo -e "\e[34m copy mongodb repo file \e[0m"
-cp mongodb.repo /etc/yum.repos.d/mongo.repo
+cp mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 echo -e "\e[34m install Mongodb server \e[0m"
-dnf install mongodb-org-shell -y
+dnf install mongodb-org-shell -y &>>/tmp/roboshop.log
 echo -e "\e[34m Loading List of products we want to sell \e[0m"
 mongo --host mongodb-dev.devopsb73.tech </app/schema/catalogue.js
