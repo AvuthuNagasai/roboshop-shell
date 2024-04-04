6829daa973b8cfa996936c00422f5b6aa2976374
nagasai <avuthunagasaireddy@gmail.com>
2024-04-04 16:42:39 +0530

    learning front end automation

--- catalogue.sh
+++ catalogue.sh
@@ -3,7 +3,7 @@ dnf module disable nodejs -y &>>/tmp/roboshop.log
 dnf module enable nodejs:18 -y &>>/tmp/roboshop.log
 echo -e "\e[34m installing nodejs \e[0m"
 dnf install nodejs -y &>>/tmp/roboshop.log
-echo -e "\e[34m adding user roboshop \e[0m"
+echo -e "\e[34m adding user \e[0m"
 useradd roboshop &>>/tmp/roboshop.log
 echo -e "\e[34m create app directory \e[0m"
 mkdir /app &>>/tmp/roboshop.log
@@ -17,12 +17,12 @@ npm install &>>/tmp/roboshop.log
 echo -e "\e[33m Setting up SystemD Catalogue Service \e[0m"
 cp /root/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
 echo -e "\e[33m starting catalogue \e[0m"
-systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl daemon-reload
 systemctl enable catalogue &>>/tmp/roboshop.log
 systemctl start catalogue &>>/tmp/roboshop.log
 echo -e "\e[34m copy mongodb repo file \e[0m"
 cp /root/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 echo -e "\e[34m install Mongodb server \e[0m"
-dnf install mongodb-org-shell -y &>>/tmp/roboshop.log &>>/tmp/roboshop.log
+dnf install mongodb-org-shell -y &>>/tmp/roboshop.log
 echo -e "\e[33m Loading List of products we want to sell \e[0m"
 mongo --host mongodb-dev.devopsb73.tech </app/schema/catalogue.js &>>/tmp/roboshop.log
