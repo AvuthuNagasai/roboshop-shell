e0ef56d44ba8c698dfd75a7ee7fdeb583b34cdc6
nagasai <avuthunagasaireddy@gmail.com>
2024-04-04 13:10:36 +0530

    learning front end automation

--- catalogue.sh
+++ catalogue.sh
@@ -14,9 +14,9 @@ unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
 echo -e "\e[34m download the dependencies \e[0m"
 cd /app
 npm install &>>/tmp/roboshop.log
-echo -e "\e[34m Setting up SystemD Catalogue Service \e[0m"
+echo -e "\e[33m Setting up SystemD Catalogue Service \e[0m"
 cp /root/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
-echo -e "\e[34m starting catalogue \e[0m"
+echo -e "\e[33m starting catalogue \e[0m"
 systemctl daemon-reload &>>/tmp/roboshop.log
 systemctl enable catalogue &>>/tmp/roboshop.log
 systemctl start catalogue &>>/tmp/roboshop.log
@@ -24,5 +24,5 @@ echo -e "\e[34m copy mongodb repo file \e[0m"
 cp /root/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 echo -e "\e[34m install Mongodb server \e[0m"
 dnf install mongodb-org-shell -y &>>/tmp/roboshop.log &>>/tmp/roboshop.log
-echo -e "\e[34m Loading List of products we want to sell \e[0m"
+echo -e "\e[33m Loading List of products we want to sell \e[0m"
 mongo --host mongodb-dev.devopsb73.tech </app/schema/catalogue.js &>>/tmp/roboshop.log
