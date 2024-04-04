3d2db969e972e57e4ee9dbb726886b77c7d87357
nagasai <avuthunagasaireddy@gmail.com>
2024-04-04 21:29:36 +0530

    learning front end automation

--- frontend.sh
+++ frontend.sh
@@ -1,24 +1,19 @@
 echo -e "\e[31m Installing NGINX \e[0m"
-dnf install nginx -y
+yum install nginx -y &>>/tmp/roboshop.log
 
-echo -e "\e[31m Start & Enable Nginx service \e[0m"
-systemctl enable nginx
-systemctl start nginx
+echo -e "\e[31m removing old app content\e[0m"
+rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
 
-echo -e "\e[31m removing old app content \e[0m"
-rm -rf /usr/share/nginx/html/*
+echo -e "\e[32m download frontend content\e[0m"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/roboshop.log
 
-echo -e "\e[32m download frontend content \e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-
-echo -e "\e[31m extracting frontend content \e[0m"
+echo -e "\e[31m extracting frontend content\e[0m"
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>/tmp/roboshop.log
 
-rm -rf /etc/nginx/default.d/roboshop.conf
-
-echo -e "\e[31m Create Nginx Reverse Proxy Configuration \e[0m"
+## we need to copy config file
 cp /root/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf
 
 echo -e "\e[31m starting NGINX server\e[0m"
-systemctl restart nginx
+systemctl enable nginx
+systemctl start nginx
