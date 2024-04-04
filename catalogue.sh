ef3beac4cff5bd479f7dd1858406e812ad51b178
nagasai <avuthunagasaireddy@gmail.com>
2024-04-04 16:48:05 +0530

    learning front end automation

--- catalogue.sh
+++ catalogue.sh
@@ -1,28 +1,41 @@
 echo -e "\e[34m enabling nodejs version 18 \e[0m"
 dnf module disable nodejs -y &>>/tmp/roboshop.log
 dnf module enable nodejs:18 -y &>>/tmp/roboshop.log
+
 echo -e "\e[34m installing nodejs \e[0m"
 dnf install nodejs -y &>>/tmp/roboshop.log
+
 echo -e "\e[34m adding user \e[0m"
 useradd roboshop &>>/tmp/roboshop.log
+
 echo -e "\e[34m create app directory \e[0m"
+rm-rf /app
 mkdir /app &>>/tmp/roboshop.log
+
+
 echo -e "\e[34m Download the application code to created app directory \e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
 cd /app
 unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
+
+
 echo -e "\e[34m download the dependencies \e[0m"
 cd /app
 npm install &>>/tmp/roboshop.log
+
 echo -e "\e[33m Setting up SystemD Catalogue Service \e[0m"
 cp /root/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
 echo -e "\e[33m starting catalogue \e[0m"
+
 systemctl daemon-reload
 systemctl enable catalogue &>>/tmp/roboshop.log
 systemctl start catalogue &>>/tmp/roboshop.log
+
 echo -e "\e[34m copy mongodb repo file \e[0m"
 cp /root/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
+
 echo -e "\e[34m install Mongodb server \e[0m"
 dnf install mongodb-org-shell -y &>>/tmp/roboshop.log
+
 echo -e "\e[33m Loading List of products we want to sell \e[0m"
 mongo --host mongodb-dev.devopsb73.tech </app/schema/catalogue.js &>>/tmp/roboshop.log
