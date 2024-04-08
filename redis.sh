9e0c687aaebad8c0af225b40022c47a69af74243
nagasai <avuthunagasaireddy@gmail.com>
2024-04-08 15:02:22 +0530

    learning front end automation

--- redis.sh
+++ redis.sh
@@ -7,7 +7,7 @@ dnf module enable redis:remi-6.2 -y &>>/tmp/roboshop.log
 echo -e "\e[34m nstall redis \e[0m"
 dnf install redis -y &>>/tmp/roboshop.log
 echo -e "\e[34m update listening address \e[0m"
-sed i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf  /etc/redis/redis.conf &>>/tmp/roboshop.log
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf  /etc/redis/redis.conf &>>/tmp/roboshop.log
 
 echo -e "\e[34m starting redis \e[0m"
 systemctl enable redis
