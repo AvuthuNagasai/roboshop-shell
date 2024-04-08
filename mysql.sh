4d839e5a638bb46f9d888ea77f6414ad9ae122ea
nagasai <avuthunagasaireddy@gmail.com>
2024-04-08 21:06:07 +0530

    learning front end automation

---
+++ mysql.sh
@@ -0,0 +1,14 @@
+echo -e "\e[33m enabling disable MySQL 8 version \e[0m"
+dnf module disable mysql -y
+
+echo -e "\e[33m enabling disable MySQL 8 version \e[0m"
+cp /root/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo
+echo -e "\e[33m Setup the MySQL5.7 repo file \e[0m"
+dnf install mysql-community-server -y
+echo -e "\e[33m Start MySQL Service \e[0m"
+systemctl enable mysql
+systemctl start mysql
+echo -e "\e[33m Start change the default root password  \e[0m"
+mysql_secure_installation --set-root-pass RoboShop@1
+echo -e "\e[33m check the new password working or not  \e[0m"
+mysql -uroot -pRoboShop@1
