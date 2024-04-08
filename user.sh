0c1edca6e344002740c25fe7ab38f79effa6a709
nagasai <avuthunagasaireddy@gmail.com>
2024-04-08 13:53:25 +0530

    learning front end automation

--- user.sh
+++ user.sh
@@ -1,30 +1,30 @@
 echo -e "\e[34m enable nodejs version 18 \e[0m"
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
+dnf module disable nodejs -y &>>/tmp/roboshop.log
+dnf module enable nodejs:18 -y &>>/tmp/roboshop.log
 echo -e "\e[34m install node js \e[0m"
-dnf install nodejs -y
+dnf install nodejs -y &>>/tmp/roboshop.log
 
 echo -e "\e[34m adding user \e[0m"
-useradd roboshop
+useradd roboshop &>>/tmp/roboshop.log
 
 echo -e "\e[34m setting up root directory \e[0m"
-mkdir /app
+mkdir /app &>>/tmp/roboshop.log
 
 echo -e "\e[34m Download the application code to created app directory \e[0m"
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip &>>/tmp/roboshop.log
 
 cd /app
-unzip /tmp/user.zip
+unzip /tmp/user.zip &>>/tmp/roboshop.log
 
 
 echo -e "\e[34m download the dependencies \e[0m"cd /app
-npm install
+npm install &>>/tmp/roboshop.log
 
 echo -e "\e[33m Setting up SystemD user Service \e[0m"
-cp /root/roboshop-shell/user.service /etc/systemd/system/user.service
+cp /root/roboshop-shell/user.service /etc/systemd/system/user.service &>>/tmp/roboshop.log
 
 
 echo -e "\e[33m starting user \e[0m"
-systemctl daemon-reload
-systemctl enable user
-systemctl start user
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable user &>>/tmp/roboshop.log
+systemctl start user &>>/tmp/roboshop.log
