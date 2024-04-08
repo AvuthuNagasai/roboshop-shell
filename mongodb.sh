13e7ca51041f6561c90124b98c04edc4340d1608
nagasai <avuthunagasaireddy@gmail.com>
2024-04-08 10:52:41 +0530

    learning front end automation

--- mongodb.sh
+++ mongodb.sh
@@ -1,12 +1,12 @@
-cp /root/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo
+cp /root/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 
-dnf install mongodb-org -y
-
-
+dnf install mongodb-org -y &>>/tmp/roboshop.log
 
 
-systemctl enable mongod
-systemctl start mongod
+
+
+systemctl enable mongod &>>/tmp/roboshop.log
+systemctl start mongod &>>/tmp/roboshop.log
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
-systemctl restart mongod
+systemctl restart mongod &>>/tmp/roboshop.log
