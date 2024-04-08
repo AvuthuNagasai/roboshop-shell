cf63ad54c7747973924747cd9ccf33b27755a819
nagasai <avuthunagasaireddy@gmail.com>
2024-04-08 11:08:10 +0530

    learning front end automation

--- catalogue.sh
+++ catalogue.sh
@@ -1,27 +1,27 @@
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
+dnf module disable nodejs -y &>>/tmp/roboshop.log
+dnf module enable nodejs:18 -y &>>/tmp/roboshop.log
 
-dnf install nodejs -y
+dnf install nodejs -y &>>/tmp/roboshop.log
 
 useradd roboshop
 
 mkdir /app
 
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
 cd /app
-unzip /tmp/catalogue.zip
+unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
 
 cd /app
-npm install
+npm install &>>/tmp/roboshop.log
 
-cp /etc/systemd/system/catalogue.service
+cp /root/roboshop-shell/mongodb.repo /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
 
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl start catalogue
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable catalogue &>>/tmp/roboshop.log
+systemctl start catalogue &>>/tmp/roboshop.log
 
-cp /etc/yum.repos.d/mongo.repo
+cp /etc/yum.repos.d/mongo.repo /etc/systemd/system/catalogue.service
 
-dnf install mongodb-org-shell -y
+dnf install mongodb-org-shell -y &>>/tmp/roboshop.log
 
-mongo --host mongodb-dev.devopsb73.tech </app/schema/catalogue.js
+mongo --host mongodb-dev.devopsb73.tech </app/schema/catalogue.js &>>/tmp/roboshop.log
