337e8838dbe3c49e6dc8f7d00dc838ad124e4b98
nagasai <avuthunagasaireddy@gmail.com>
2024-04-08 21:47:01 +0530

    learning front end automation

--- shipping.sh
+++ shipping.sh
@@ -19,11 +19,6 @@ cd /app
 mvn clean package &>>/tmp/roboshop.log
 mv target/shipping-1.0.jar shipping.jar &>>/tmp/roboshop.log
 
-echo -e "\e[33m installing mysql \e[0m"
-dnf install mysql -y &>>/tmp/roboshop.log
-echo -e "\e[33m Loading Schema \e[0m"
-mysql -h mysql-dev.devopsb73.tech -uroot -pRoboShop@1 < /app/schema/shipping.sql
-
 echo -e "\e[33m Setting up SystemD shipping Service \e[0m"
 cp /root/roboshop-shell/shipping.service /etc/systemd/system/shipping.service &>>/tmp/roboshop.log
 
@@ -31,3 +26,11 @@ echo -e "\e[33m starting shipping \e[0m"
 systemctl daemon-reload
 systemctl enable shipping &>>/tmp/roboshop.log
 systemctl start shipping &>>/tmp/roboshop.log
+
+echo -e "\e[33m installing mysql \e[0m"
+dnf install mysql -y &>>/tmp/roboshop.log
+echo -e "\e[33m Loading Schema \e[0m"
+mysql -h mysql-dev.devopsb73.tech -uroot -pRoboShop@1 < /app/schema/shipping.sql
+
+echo -e "\e[33m starting shipping \e[0m"
+systemctl restart shipping
