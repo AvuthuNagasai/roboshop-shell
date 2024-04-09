875c9a237842aebdc7f456b6b6fe64f4cdb4c3df
nagasai <avuthunagasaireddy@gmail.com>
2024-04-09 19:32:55 +0530

    learning front end automation

--- shipping.sh
+++ shipping.sh
@@ -1,13 +1,11 @@
-
 echo -e "\e[33m installing maven \e[0m"
-dnf install maven -y &>>/tmp/roboshop.log
+
+dnf install maven -y
 
 echo -e "\e[33m adding user \e[0m"
-useradd roboshop &>>/tmp/roboshop.log
-
+useradd roboshop
 echo -e "\e[34m create app directory \e[0m"
-mkdir /app &>>/tmp/roboshop.log
-
+mkdir /app
 
 echo -e "\e[33m Download the application code to created app directory \e[0m"
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
@@ -16,21 +14,23 @@ unzip /tmp/shipping.zip
 
 echo -e "\e[33m download the dependencies & build the application \e[0m"
 cd /app
-mvn clean package &>>/tmp/roboshop.log
-mv target/shipping-1.0.jar shipping.jar &>>/tmp/roboshop.log
+mvn clean package
+mv target/shipping-1.0.jar shipping.jar
 
 echo -e "\e[33m Setting up SystemD shipping Service \e[0m"
-cp /root/roboshop-shell/shipping.service /etc/systemd/system/shipping.service &>>/tmp/roboshop.log
+cp /root/roboshop-shell/shipping.service /etc/systemd/system/shipping.service
 
 echo -e "\e[33m starting shipping \e[0m"
 systemctl daemon-reload
-systemctl enable shipping &>>/tmp/roboshop.log
-systemctl start shipping &>>/tmp/roboshop.log
+systemctl enable shipping
+systemctl start shipping
 
 echo -e "\e[33m installing mysql \e[0m"
-dnf install mysql -y &>>/tmp/roboshop.log
+dnf install mysql -y
+
 echo -e "\e[33m Loading Schema \e[0m"
 mysql -h mysql-dev.devopsb73.tech -uroot -pRoboShop@1 < /app/schema/shipping.sql
 
 echo -e "\e[33m starting shipping \e[0m"
 systemctl restart shipping
+
