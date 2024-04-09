493ec2ef9f658dddfcbc4493ee8bdd37f03d8e0f
nagasai <avuthunagasaireddy@gmail.com>
2024-04-09 22:12:32 +0530

    learning front end automation

--- shipping.sh
+++ shipping.sh
@@ -4,7 +4,9 @@ dnf install maven -y &>>/tmp/roboshop.log
 
 echo -e "\e[33m adding user \e[0m"
 useradd roboshop &>>/tmp/roboshop.log
+
 echo -e "\e[34m create app directory \e[0m"
+rm -rf /app
 mkdir /app &>>/tmp/roboshop.log
 
 echo -e "\e[33m Download the application code to created app directory \e[0m"
@@ -17,6 +19,12 @@ cd /app
 mvn clean package &>>/tmp/roboshop.log
 mv target/shipping-1.0.jar shipping.jar &>>/tmp/roboshop.log
 
+echo -e "\e[33m installing mysql \e[0m"
+dnf install mysql -y &>>/tmp/roboshop.log
+
+echo -e "\e[33m Loading Schema \e[0m"
+mysql -h mysql-dev.devopsb73.tech -uroot -pRoboShop@1 < /app/schema/shipping.sql
+
 echo -e "\e[33m Setting up SystemD shipping Service \e[0m"
 cp /root/roboshop-shell/shipping.service /etc/systemd/system/shipping.service &>>/tmp/roboshop.log
 
@@ -25,12 +33,4 @@ systemctl daemon-reload &>>/tmp/roboshop.log
 systemctl enable shipping &>>/tmp/roboshop.log
 systemctl start shipping &>>/tmp/roboshop.log
 
-echo -e "\e[33m installing mysql \e[0m"
-dnf install mysql -y &>>/tmp/roboshop.log
-
-echo -e "\e[33m Loading Schema \e[0m"
-mysql -h mysql-dev.devopsb73.tech -uroot -pRoboShop@1 < /app/schema/shipping.sql
-
-echo -e "\e[33m starting shipping \e[0m"
-systemctl restart shipping &>>/tmp/roboshop.log
 
