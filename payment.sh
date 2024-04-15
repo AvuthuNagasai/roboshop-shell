ea28c7548dabd646422eb0520cd90dbca53f56ec
nagasai <avuthunagasaireddy@gmail.com>
2024-04-15 21:22:11 +0530

    learning front end automation

--- payment.sh
+++ payment.sh
@@ -1,24 +1,24 @@
 echo -e "\e[33m installing python \e[0m"
-dnf install python36 gcc python3-devel -y
+dnf install python36 gcc python3-devel -y &>>/tmp/roboshop.log
 
 echo -e "\e[34m adding user \e[0m"
-useradd roboshop
+useradd roboshop &>>/tmp/roboshop.log
 
-mkdir /app
+mkdir /app &>>/tmp/roboshop.log
 
 echo -e "\e[34m Download the application code to created app directory \e[0m"
-curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
+curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip &>>/tmp/roboshop.log
 cd /app
-unzip /tmp/payment.zip
+unzip /tmp/payment.zip &>>/tmp/roboshop.log
 
 echo -e "\e[34m Lets download the dependencies \e[0m"
 cd /app
-pip3.6 install -r requirements.txt
+pip3.6 install -r requirements.txt &>>/tmp/roboshop.log
 
 echo -e "\e[34m Setup SystemD Payment Service \e[0m"
-cp /root/roboshop-shell/payment.service /etc/systemd/system/payment.service
+cp /root/roboshop-shell/payment.service /etc/systemd/system/payment.service &>>/tmp/roboshop.log
 
 echo -e "\e[34m Start payment service \e[0m"
 systemctl daemon-reload
-systemctl enable payment
-systemctl start payment
+systemctl enable payment &>>/tmp/roboshop.log
+systemctl start payment &>>/tmp/roboshop.log
